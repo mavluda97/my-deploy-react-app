903872b8cb909182793ff03b96cf36039b0858b5
mavluda27 <mavluda97@gmail.com>
2020-10-25 13:17:00 +0500

    Updates

---
+++ precache-manifest.aa683d26d66df12a2e73427b01d66bb2.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f145c977351e849b588449c1e3682023",
+    "url": "/my-deploy-react-app/index.html"
+  },
+  {
+    "revision": "f4e865d774db1bed6e74",
+    "url": "/my-deploy-react-app/static/css/main.940c1aa4.chunk.css"
+  },
+  {
+    "revision": "be97eb227d7a3b1449c5",
+    "url": "/my-deploy-react-app/static/js/2.6b6a6e7b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-deploy-react-app/static/js/2.6b6a6e7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4e865d774db1bed6e74",
+    "url": "/my-deploy-react-app/static/js/main.06fb8da3.chunk.js"
+  },
+  {
+    "revision": "3c80c6d3ba0438ff2ba7",
+    "url": "/my-deploy-react-app/static/js/runtime-main.7ea412c4.js"
+  },
+  {
+    "revision": "aebaec51b2f82a2993fda821ca04e228",
+    "url": "/my-deploy-react-app/static/media/defaultBcg.aebaec51.jpeg"
+  },
+  {
+    "revision": "33ab9f1e88f204b5b8a13725a918f36c",
+    "url": "/my-deploy-react-app/static/media/details-1.33ab9f1e.jpeg"
+  },
+  {
+    "revision": "50c23e6265e40d602f1ac080e2d4169a",
+    "url": "/my-deploy-react-app/static/media/details-2.50c23e62.jpeg"
+  },
+  {
+    "revision": "7345b9f61680c799868f66f297cedab5",
+    "url": "/my-deploy-react-app/static/media/details-3.7345b9f6.jpeg"
+  },
+  {
+    "revision": "7569a31606be3db6b229327eec5999df",
+    "url": "/my-deploy-react-app/static/media/details-4.7569a316.jpeg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/my-deploy-react-app/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-deploy-react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9f0cccc1e406a5aa9ef6ee36229762c3",
+    "url": "/my-deploy-react-app/static/media/logo.9f0cccc1.svg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/my-deploy-react-app/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "3b5c08716be6b713154c8aedaec3f5bf",
+    "url": "/my-deploy-react-app/static/media/room-10.3b5c0871.jpeg"
+  },
+  {
+    "revision": "f08fee37bdc8f3d11ef36658cfacc44e",
+    "url": "/my-deploy-react-app/static/media/room-11.f08fee37.jpeg"
+  },
+  {
+    "revision": "0e293352532d9483616fe4c5110c8e28",
+    "url": "/my-deploy-react-app/static/media/room-12.0e293352.jpeg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/my-deploy-react-app/static/media/room-2.052c7681.jpeg"
+  },
+  {
+    "revision": "9a1103b597895b4c4b373d6304400781",
+    "url": "/my-deploy-react-app/static/media/room-3.9a1103b5.jpeg"
+  },
+  {
+    "revision": "1e407d88a352a0e8493e2275a76f39f5",
+    "url": "/my-deploy-react-app/static/media/room-4.1e407d88.jpeg"
+  },
+  {
+    "revision": "a5c06e0bb3e90f927159439d68b07253",
+    "url": "/my-deploy-react-app/static/media/room-5.a5c06e0b.jpeg"
+  },
+  {
+    "revision": "b9a34e969ab86c1500d67183f646995a",
+    "url": "/my-deploy-react-app/static/media/room-6.b9a34e96.jpeg"
+  },
+  {
+    "revision": "e251c8d571a7f753b3da985e4b8a34c6",
+    "url": "/my-deploy-react-app/static/media/room-7.e251c8d5.jpeg"
+  },
+  {
+    "revision": "ebb7fca5f456fb07e0096ab3ef72b330",
+    "url": "/my-deploy-react-app/static/media/room-8.ebb7fca5.jpeg"
+  },
+  {
+    "revision": "7b17c49f01aa17fc11df8400d604e1c9",
+    "url": "/my-deploy-react-app/static/media/room-9.7b17c49f.jpeg"
+  }
+]);
